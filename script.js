fa9feadb995618bd942aaca384a152198746d6d9
AbhinavKR <kumarabhinav.akr@gmail.com>
2024-10-02 22:56:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleSkills() {
+    const skillsContent = document.getElementById('skills-content');
+    skillsContent.style.display = skillsContent.style.display === 'none' ? 'block' : 'none';
+}
+
+function toggleInterests() {
+    const interestsContent = document.getElementById('interests-content');
+    interestsContent.style.display = interestsContent.style.display === 'none' ? 'block' : 'none';
+}
+
+function toggleAboutMe() {
+    const aboutMeContent = document.getElementById('about-me-content');
+    aboutMeContent.style.display = aboutMeContent.style.display === 'none' ? 'block' : 'none';
+}
